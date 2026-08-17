dce8a26053dd43a6e2b1a7cc03d525ade9252ccb
admin_king <15304530821@163.com>
2026-08-17 17:56:31 +0800

    初始化

---
+++ src/main/java/com/king/aicustomerservice/AiCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.king.aicustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/king/aicustomerservice/AiCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.aicustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
